9699df59e3b1c77601c2442da1f2b3a051a99299
ryansyah <achdiadsyah@gmail.com>
2021-12-07 22:48:12 +0700

    Change Nav Color and Other

--- application/views/pengumuman/reguler.php
+++ application/views/pengumuman/reguler.php
@@ -3,7 +3,14 @@
                         <div class="col-lg-12">
                             <div class="card">
                                 <div class="card-body">
+                                    <?php if(check_open(psb_detail("pengumuman_reguler"), psb_detail("tutup_daftar_ulang_reguler")) == "Open"): ?>
                                     
+                                    <?php else: ?>
+                                        <center>
+                                            <p class="lead">Pengumuman Belum Di Buka</p>
+                                            <p>Pengumuman Jalur Reguler Tanggal : <?= date_indo(psb_detail("pengumuman_reguler")); ?></p>
+                                        </center>
+                                    <?php endif; ?>
                                 </div>
                             </div>
                         </div>
