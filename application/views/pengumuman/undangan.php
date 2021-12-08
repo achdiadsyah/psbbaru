1f57c5c4c24abeb4824e5776ca72e4b92dc624b9
ryansyah <achdiadsyah@gmail.com>
2021-12-09 04:23:04 +0700

    Bak Na salah lah

--- application/views/pengumuman/undangan.php
+++ application/views/pengumuman/undangan.php
@@ -8,7 +8,7 @@
                                     <?php else: ?>
                                         <center>
                                             <p class="lead">Pengumuman Belum Di Buka</p>
-                                            <p>Pengumuman Jalur Undangan Tanggal : <?= date_indo(psb_detail("pengumuman_reguler")); ?></p>
+                                            <p>Pengumuman Jalur Undangan Tanggal : <?= date_indo(psb_detail("pengumuman_undangan")); ?></p>
                                         </center>
                                     <?php endif; ?>
                                 </div>
