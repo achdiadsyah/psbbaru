099788488abf03f4547c3a00540169754d041982
ryansyah <achdiadsyah@gmail.com>
2021-12-21 15:39:21 +0700

    Set Reuired to Akademik

--- application/views/auth/js-register.php
+++ application/views/auth/js-register.php
@@ -5,19 +5,21 @@
                 if($(this).val() == 'undangan'){
                     $('#kode_title').removeClass('d-none');
                     $('#kode_form').removeClass('d-none');
-
-                    $("#kode_undangan").prop('required',true);
-
+                        
                     $('#akademi_title').removeClass('d-none');
                     $('#akademi_form').removeClass('d-none');
+
+                    $("#kode_undangan").prop('required',true);
+                    $("#s_akademik").prop('required',true);
                 } else  {
                     $('#kode_title').addClass('d-none');
                     $('#kode_form').addClass('d-none');
 
-                    $("#kode_undangan").prop('required',false);
-
                     $('#akademi_title').addClass('d-none');
                     $('#akademi_form').addClass('d-none');
+                    
+                    $("#kode_undangan").prop('required',false);
+                    $("#s_akademik").prop('required',false);
                 }
             });
 
