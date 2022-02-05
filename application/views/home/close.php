9bb45d5620bb987051968d07b829133f7d350c5a
ryansyah <achdiadsyah@gmail.com>
2022-02-05 22:14:41 +0700

    Typo

--- application/views/home/close.php
+++ application/views/home/close.php
@@ -5,7 +5,7 @@
                         <div class="card text-left">
                           <div class="card-body">
                             <center>
-                              <h3 class="card-text">Pengumuaman PSB 2021/2022 Telah di buka.
+                              <h3 class="card-text">Pengumuaman PSB 2022/2023 Telah di buka.
                                 <br>Silahkan Login untuk melihat pengumuman atau mendaftar ulang</h3>
                                 <h4>Atau</h4>
                                 <a href="<?= base_url('assets/file/pengumuman2022.pdf'); ?>" target="_blank" class="btn btn-primary">Lihat Pengumuman Resmi</a>
